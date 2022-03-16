4d6b734b344ec198306128eafd5ae0b8407c90cf
ZxHv <zxh19960413@sina.com>
2022-03-16 16:41:50 +0800

    self-chapter1-3

---
+++ self-chapter1-3/src/main/java/com/xxexample/selfchapter13/web/HelloController.java
@@ -0,0 +1,9 @@
+package com.xxexample.selfchapter13.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    private String db;
+}
