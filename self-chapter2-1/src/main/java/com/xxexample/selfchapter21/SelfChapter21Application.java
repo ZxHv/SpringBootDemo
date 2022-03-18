1e696f3aec92400978d4151790305cb55974a54f
ZxHv <zxh19960413@sina.com>
2022-03-18 10:11:57 +0800

    self-chapter2-1

---
+++ self-chapter2-1/src/main/java/com/xxexample/selfchapter21/SelfChapter21Application.java
@@ -0,0 +1,13 @@
+package com.xxexample.selfchapter21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfChapter21Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelfChapter21Application.class, args);
+    }
+
+}
